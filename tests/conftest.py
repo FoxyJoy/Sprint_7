62896d238e7d909855ae09ec9d384d2dc108b6d1
Olesya S. <unclled@gmail.com>
2025-02-14 12:44:41 +0300

    Fix 1

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from api.api_courier import *
-
-# Создание API метода
-@pytest.fixture
-def register():
-    register = register_new_courier_and_return_login_password()
-    yield register
-    CourierAPI.delete_courier(register[0], register[1])
